a11265189cc8c18f3c95bf846d30acb2bb4e1e3d
Tyler Syrowitz <tsyrowitz@gmail.com>
2017-11-12 22:44:16 +0000

    Added province model, associations, references, active admin, some provinces, and updated partial. Part 3

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,5 @@ class Customer < ApplicationRecord
   validates :full_name, presence: true
   validates :full_name, uniqueness: true
   validates :phone_number, presence: true
+  belongs_to :province
 end
